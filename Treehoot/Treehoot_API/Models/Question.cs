fb7162f572afcd403468b172ab32c9a2dea4fe37
Nojus <nojus.dziaugys@gmail.com>
2023-09-14 17:49:39 +0300

    question model class beacause previous commit didn't contain it lol

--- Treehoot/Treehoot_API/Models/Question.cs
+++ Treehoot/Treehoot_API/Models/Question.cs
@@ -1,6 +1,10 @@
-﻿namespace Treehoot_API.Models
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Treehoot_API.Models;
+
+public class Question
 {
-    public class Question
-    {
-    }
+    public long Id { get; set; }
+    public string ? QuestionSentence{ get; set; }
+    public Dictionary <String, Boolean> ? Answers { get; set; }
 }
