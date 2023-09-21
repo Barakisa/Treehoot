1ce564a8a589ba1292c3f1c232421dac799c6a3f
rytis17 <tritis02@gmail.com>
2023-09-22 00:37:23 +0300

    Added user class and controller

---
+++ Treehoot/Treehoot_API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Treehoot_API.Models;
+
+namespace Treehoot_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult<Question> Get(Guid Id)
+        {
+            return Ok();
+        }
+    }
+}
