4ac1d3e167b231b8f6d41bd8ae54a7abb60cbf32
Nojus <nojus.dziaugys@gmail.com>
2023-09-21 18:25:43 +0300

    removed experimental stuff

--- Treehoot/Treehoot_API/Controllers/StageController.cs
+++ Treehoot/Treehoot_API/Controllers/StageController.cs
@@ -1,9 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
-using Treehoot_API.Models;
-using System;
-using System.IO;
-using System.Text.Json;
 
 namespace Treehoot_API.Controllers
 {
@@ -11,33 +6,6 @@ namespace Treehoot_API.Controllers
     [Route("[controller]")]
     public class StageController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private Quiz[] GetStagesFromDB()
-        {
-            string filePath = "path_to_your_json_file.json";
-
-            try
-            {
-                string jsonContent = File.ReadAllText(filePath);
-
-                // Deserialize the JSON content into your object
-                YourObjectType obj = JsonSerializer.Deserialize<YourObjectType>(jsonContent);
-
-                // Now 'obj' contains the deserialized data from your JSON file
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("File not found.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
-        }
 
         private readonly ILogger<StageController> _logger;
 
