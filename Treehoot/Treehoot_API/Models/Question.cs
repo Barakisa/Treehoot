ce6535359187fe4d9415a5f7dc199016dc21c618
liepa1 <liepaite02@gmail.com>
2023-09-21 22:54:23 +0300

    Created question controller Created react project Edited model classes

--- Treehoot/Treehoot_API/Models/Question.cs
+++ Treehoot/Treehoot_API/Models/Question.cs
@@ -4,7 +4,14 @@ namespace Treehoot_API.Models;
 
 public class Question
 {
-    public long Guid { get; set; }
+    public Question(int id, string questionText, List<Answer> answers)
+    {
+        Id = id;
+        QuestionText = questionText;
+        Answers = answers;
+    }
+
+    public int Id { get; set; }
     public string QuestionText{ get; set; }
-    public Answer[]? Answers { get; set; }
+    public List<Answer> Answers { get; set; }
 }
--- Treehoot/Treehoot_API/Models/Stage.cs
+++ Treehoot/Treehoot_API/Models/Stage.cs
@@ -2,9 +2,17 @@
 
 public class Stage
 {
-    public Guid Id { get; set; }
+
+    public Stage(int id, string name, List<Question> questions)
+    {
+        Id = id;
+        Name = name;
+        Questions = questions;
+    }
+
+    public int Id { get; set; }
 
     public string Name { get; set; }
-    public Question[] Questions { get; set; }
+    public List<Question> Questions { get; set; }
 }
 
