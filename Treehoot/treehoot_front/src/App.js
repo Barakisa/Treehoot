b26f0fa4f644facacc01f855ab5b345653cd9cd0
liepa1 <liepaite02@gmail.com>
2023-09-27 20:56:02 +0300

    added router to app.js

--- Treehoot/treehoot_front/src/App.js
+++ Treehoot/treehoot_front/src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import Question from "./components/Question/Question.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home.js";
 
 function App() {
   return (
-    <div className="p-2">
-      <Question />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/question"
+          element={
+            <div classname="p-2">
+              <Question />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
