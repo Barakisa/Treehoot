ec6d6022e6f9a4681dd9f971839437c26534c3bf
rytis17 <tritis02@gmail.com>
2023-09-28 22:17:36 +0300

    Added DTOs to DAL and DbContext

---
+++ Treehoot/Treehoot_DataAccess/DTOs/Quiz.cs
@@ -0,0 +1,11 @@
+﻿namespace Treehoot_API.Models;
+
+public class Quiz
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Timer { get; set; }
+    public List<Stage> Stages { get; set; }
+}
---
+++ Treehoot/Treehoot_DataAccess/DTOs/Stage.cs
@@ -0,0 +1,10 @@
+﻿namespace Treehoot_API.Models;
+
+public class Stage
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public List<int> Questions { get; set; }
+}
+
