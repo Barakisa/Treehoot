b843d7944e93b386dfb47f6e9d98dd5e55d06e18
rytis17 <tritis02@gmail.com>
2023-10-05 21:34:10 +0300

    Added quiz controller

---
+++ Treehoot/Treehoot_API/Controllers/QuizController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Treehoot_API.Models;
+
+namespace Treehoot_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetSpecificQuiz")]
+        public ActionResult<Quiz> Get(int quizId)
+        {
+            try
+            {
+
+                var jsonText = System.IO.File.ReadAllText("FakeDb/FakeDb1.json");
+
+                var data = JsonSerializer.Deserialize<JsonConversion>(jsonText);
+                var allQuizes = data.Quizes.ToList();
+
+                var quiz = allQuizes.FirstOrDefault(q => q.Id == quizId);
+
+                return Ok(quiz);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+                return BadRequest("File not found");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e.Message}");
+            }
+        }
+    }
+}
--- Treehoot/Treehoot_API/Models/Question.cs
+++ Treehoot/Treehoot_API/Models/Question.cs
@@ -5,5 +5,5 @@ public class Question
     public int Id { get; set; }
     
     public string QuestionText{ get; set; }
-    public List<int> Answers { get; set; }
+    public List<Answer> Answers { get; set; }
 }
