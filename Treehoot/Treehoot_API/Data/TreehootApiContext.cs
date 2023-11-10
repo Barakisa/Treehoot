630e7adde59267c29e643fc75375ea08e6b0ea1d
Nojus <nojus.dziaugys@gmail.com>
2023-11-10 02:04:51 +0200

    started working on ef, db, also renamed project properly. There are a lot of new packages in api project...

---
+++ Treehoot/Treehoot_API/Data/TreehootApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Treehoot.Domain.Models;
+
+namespace Treehoot.Api.Data
+{
+    public class TreehootApiContext : DbContext
+    {
+        public TreehootApiContext (DbContextOptions<TreehootApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Treehoot.Domain.Models.Answer> Answer { get; set; } = default!;
+    }
+}
