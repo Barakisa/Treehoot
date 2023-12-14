7a75315884867bcf17527d967c9db8dbabfc0be4
Nojus <nojus.dziaugys@gmail.com>
2023-12-14 20:18:08 +0200

    updated user model, added user service with log in and register methods

---
+++ Treehoot/Treehoot.Application/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Treehoot.Application.Data;
+using Treehoot.Domain.Models;
+
+
+namespace Treehoot.Application.Services
+{
+    internal class UserService
+    {
+        private readonly TreehootApiContext _treehootApiContext;
+
+        public UserService(TreehootApiContext treehootApiContext)
+        {
+            _treehootApiContext = treehootApiContext;
+        }
+
+        public async Task<User> LogIn(string username, string password)
+        {
+            var user = await _treehootApiContext.User
+                                                .FirstOrDefaultAsync(user => (user.Email == username) ||
+                                                                    (user.Name == username))
+            if (user == null || user.Password != password)
+            {
+                return new User();
+            }
+            else
+            {
+                return user;
+            }
+        }
+
+        public async Task<User> Register(string username, string password)
+        {
+            var userId = new Guid();
+            _treehootApiContext.User.Add(new User() { Id = userId, 
+                                                            Name = username, 
+                                                            Email = username, 
+                                                            Password = password });
+            await _treehootApiContext.SaveChangesAsync();
+            var newUser = await _treehootApiContext.User.FirstOrDefaultAsync(user => user.Id == userId);
+            
+            if (newUser == null)
+            {
+                return new User();
+            }
+
+            return newUser;
+        }
+    }
+}
--- Treehoot/Treehoot.Domain/Models/User.cs
+++ Treehoot/Treehoot.Domain/Models/User.cs
@@ -6,6 +6,6 @@ public record User
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-
+    public string Password { get; set; }
 }
 
